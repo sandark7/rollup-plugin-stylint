956b45a5d9594bc625dde762f6ecc65e753278b8
an.smirnov <sandark7@gmail.com>
2017-04-20 16:38:18 +0300

    Added functionality

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,12 @@ const packageJson = require('./package.json');
 
 export default {
   entry: 'src/index.js',
-  external: Object.keys(packageJson.dependencies).concat(['path']),
+  external: Object.keys(packageJson.dependencies),
   targets: [{
     format: 'cjs',
     dest: packageJson['main']
   }, {
     format: 'es',
     dest: packageJson['module']
-  }],
-  sourceMap: 'inline'
+  }]
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 import { createFilter } from 'rollup-pluginutils';
-// import stylint from 'stylint';
+import lint from 'stylint';
 
 export default function stylint(options = {}) {
-  const filter = createFilter(options.include, options.exclude || 'node_modules/**');
+  const filter = createFilter(
+    options.include || '**/*.styl',
+    options.exclude || 'node_modules/**'
+  );
 
   return {
     name: 'stylint',
+
     transform(code, id) {
-      console.log(code, id);
+      if (!filter(id)) {
+        return null;
+      }
+
+      lint(code, options.config).methods({
+        read: function() {
+          this.cache.files = [ id ];
+          this.parse(null, [ code ]);
+        },
+        done: function() {
+          if (!this.cache.errs.length && !this.cache.warnings.length) {
+            return;
+          }
+
+          if (this.state.quiet) {
+            throw new Error(`Build failed due to:\n${this.cache.msg}`);
+          } else {
+            let result = [
+              ...this.cache.errs,
+              ...this.cache.warnings
+            ].join('\n\n')
+
+            let output = options.reporter ? this.cache.msg : `\n${result}\n\n${this.cache.msg}\n`;
+
+            console.log(output);
+          }
+        }
+      }).create({}, {
+        config: options.configFile,
+        strict: options.strict,
+        reporter: options.reporter
+      });
     }
   }
 }
